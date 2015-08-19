04bf91f5f38ff5154ebec8cbd0ad834efe8a5afb
Nikolay Kozlov <nikolay@velkomb.com>
2015-08-19 17:07:49 +0500

    added all of the images for project

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap 101 Template</title>
-    <link href="_/css/bootstrap.min.css" rel="stylesheet">
-    <link href="_/css/mystyles.min.css" rel="stylesheet">
+    <link href="_/css/bootstrap.css" rel="stylesheet">
+    <link href="_/css/mystyles.css" rel="stylesheet">
   </head>
   <body>
     <h1>Hello, world!</h1>
